ad0e3db735a112c1d626c6a7d038cc8a7e24e90e
Sebastian J <zeeman@zeeman.de>
2020-04-05 15:05:23 -0400

    Adding all the lights :)

--- src/javascript/iot2influxdb.js
+++ src/javascript/iot2influxdb.js
@@ -19,8 +19,15 @@ exports.handler = async (event, context, callback) => {
   */
     var result = await writeToInfluxDB(
         reported.temperature, 
+        reported.pressure,
         reported.lux1, 
         reported.lux2,
+        reported.lux3,
+        reported.lux4,
+        reported.lux5,
+        reported.lux6,
+        reported.lux7,
+        reported.lux8,
         reported.light);
 
     console.log("Finished executing", result);
@@ -28,7 +35,7 @@ exports.handler = async (event, context, callback) => {
     return callback(null, result);
   };
 
-function writeToInfluxDB(celsius, lux1, lux2, light_state)
+function writeToInfluxDB(temperature, pressure, lux1, lux2, lux3, lux4, lux5, lux6, lux7, lux8, light_state)
 {
     console.log("Executing Influx insert");
 
@@ -45,6 +52,15 @@ function writeToInfluxDB(celsius, lux1, lux2, light_state)
                 celsius: Influx.FieldType.FLOAT
             },
     
+            tags: []
+        },
+        {
+            measurement: 'pressure',
+    
+            fields: {
+                pascal: Influx.FieldType.FLOAT
+            },
+
             tags: []
         },
         {
@@ -70,7 +86,10 @@ function writeToInfluxDB(celsius, lux1, lux2, light_state)
     
     const resultPromise = client.writePoints([
         {
-            measurement: 'temperature', fields: { celsius: celsius }
+            measurement: 'temperature', fields: { celsius: temperature }
+        },
+        {
+            measurement: 'pressure', fields: { pascal: pressure }
         },
         {
             measurement: 'lights', fields: { lux: lux1 },
@@ -81,7 +100,31 @@ function writeToInfluxDB(celsius, lux1, lux2, light_state)
             tags: { sensor: "sensor2"}
         },
         {
-            measurement: 'light', fields: { state: light_state === true }
+            measurement: 'lights', fields: { lux: lux3 },
+            tags: { sensor: "sensor3"}
+        },
+        {
+            measurement: 'lights', fields: { lux: lux4 },
+            tags: { sensor: "sensor4"}
+        },
+        {
+            measurement: 'lights', fields: { lux: lux5 },
+            tags: { sensor: "sensor5"}
+        },
+        {
+            measurement: 'lights', fields: { lux: lux6 },
+            tags: { sensor: "sensor6"}
+        },
+        {
+            measurement: 'lights', fields: { lux: lux7 },
+            tags: { sensor: "sensor7"}
+        },
+        {
+            measurement: 'lights', fields: { lux: lux8 },
+            tags: { sensor: "sensor8"}
+        },
+        {
+            measurement: 'light', fields: { state: light_state === true ? 1 : 0}
         }
     ]) 
 
